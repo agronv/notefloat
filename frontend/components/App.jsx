736f692813d11a52f736b464f4347a70faf355a6
agronv <avelaj96@gmail.com>
2019-01-21 20:38:15 -0500

    refactor a little bit

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,18 +1,26 @@
 
 import React from "react";
 import NavBar from "./navbar";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
 import { AuthRoute } from '../utils/route_util';
+import Splash from './splash/splash';
+import PlayBar from './playbar/PlayBar';
 
 const App = () => (
   <>
     <header>
       <Route path="/" component={NavBar} />
     </header>
-      <AuthRoute path="/signup" component={SignUpFormContainer} />
-      <AuthRoute path="/login" component={LogInFormContainer} />
+      <Switch>
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        <AuthRoute exact path="/login" component={LogInFormContainer} />
+      </Switch>
+      {/* <Route exact path="/" component={Splash}/> */}
+    <footer>
+      {/* <Route exact path="/" component={PlayBar} />       */}
+    </footer>
   </>
 );
 
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import React from 'react';
+
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Splash;
