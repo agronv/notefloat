0fedc3dfcc1ccb4a51271aa47be1c43770f7be19
agronv <avelaj96@gmail.com>
2019-01-22 18:04:27 -0500

    update

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,8 +5,6 @@ import { Route, Switch } from "react-router-dom";
 import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
 import { AuthRoute } from '../utils/route_util';
-import Splash from './splash/splash';
-import PlayBar from './playbar/PlayBar';
 
 const App = () => (
   <>
@@ -17,9 +15,7 @@ const App = () => (
         <AuthRoute exact path="/signup" component={SignUpFormContainer} />
         <AuthRoute exact path="/login" component={LogInFormContainer} />
       </Switch>
-      {/* <Route exact path="/" component={Splash}/> */}
     <footer>
-      {/* <Route exact path="/" component={PlayBar} />       */}
     </footer>
   </>
 );
